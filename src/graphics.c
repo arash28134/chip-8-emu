11c858edae0d6edb977442918f58f25297e8fe54
Ali Atashrooz <atashroozarash@gmail.com>
2024-06-22 12:28:38 +0330

    Update

--- src/graphics.c
+++ src/graphics.c
@@ -147,82 +147,82 @@ void loop() {
 					switch (event.key.keysym.sym) {
 						case SDLK_x:
 							{
-								keypad[0] = 1;
+								keypad[0] = 0;
 						} break;
 
 						case SDLK_1:
 							{
-								keypad[1] = 1;
+								keypad[1] = 0;
 						} break;
 
 						case SDLK_2:
 							{
-								keypad[2] = 1;
+								keypad[2] = 0;
 						} break;
 
 						case SDLK_3:
 							{
-								keypad[3] = 1;
+								keypad[3] = 0;
 						} break;
 
 						case SDLK_q:
 							{
-								keypad[4] = 1;
+								keypad[4] = 0;
 						} break;
 
 						case SDLK_w:
 							{
-								keypad[5] = 1;
+								keypad[5] = 0;
 						} break;
 
 						case SDLK_e:
 							{
-								keypad[6] = 1;
+								keypad[6] = 0;
 						} break;
 
 						case SDLK_a:
 							{
-								keypad[7] = 1;
+								keypad[7] = 0;
 						} break;
 
 						case SDLK_s:
 							{
-								keypad[8] = 1;
+								keypad[8] = 0;
 						} break;
 
 						case SDLK_d:
 							{
-								keypad[9] = 1;
+								keypad[9] = 0;
 						} break;
 
 						case SDLK_z:
 							{
-								keypad[0xA] = 1;
+								keypad[0xA] = 0;
 						} break;
 
 						case SDLK_c:
 							{
-								keypad[0xB] = 1;
+								keypad[0xB] = 0;
 						} break;
 
 						case SDLK_4:
 							{
-								keypad[0xC] = 1;
+								keypad[0xC] = 0;
 						} break;
 
 						case SDLK_r:
 							{
-								keypad[0xD] = 1;
+								keypad[0xD] = 0;
 						} break;
 
 						case SDLK_f:
 							{
-								keypad[0xE] = 1;
+								keypad[0xE] = 0;
 						} break;
 
 						case SDLK_v:
 							{
-								keypad[0xF] = 1;
+								keypad[0xF] = 0;
 						} break;
 
 				} break;
