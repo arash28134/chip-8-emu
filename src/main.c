6062700bee3a47412b68deaabd090d20906044b2
Ali Atashrooz <atashroozarash@gmail.com>
2024-06-22 13:55:52 +0330

    Update

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 int main(int argc, char **argv){
 	char* endptr;
 	int video_scale = strtol(argv[2], &endptr, 10);
-	int frequency = strtol(argv[3], &endptr, 10);
+	float frequency = strtol(argv[3], &endptr, 10);
 	load_rom(argv[1]);    
 
 	init(VIDEO_WIDTH * video_scale, VIDEO_HEIGHT * video_scale, frequency);
